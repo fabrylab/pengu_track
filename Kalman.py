8296299e601b9b1a39e8e8f3a7233a29e38480ba
RedHulk94 <55278900+AlexanderWinterl@users.noreply.github.com>
2016-09-21 16:15:00 +0200

    Speed Up for Particle Filter. Filter Testing now in new script.

--- Kalman.py
+++ Kalman.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, division
 import numpy as np
 import scipy.stats as ss
+from timeit import default_timer as timer
 
 
 
@@ -145,12 +146,12 @@ class MultiKalman(object):
             probability_gain[:, maxj] = np.nan
             self.ActiveFilters[maxj].update(z[maxi])
 
-        X = []
-        P = []
+        x = []
+        p = []
         for kal in self.Filters:
-            X.append(kal.x[max(kal.x.keys())])
-            P.append(kal.p[max(kal.x.keys())])
-        return X, P
+            x.append(kal.x[max(kal.x.keys())])
+            p.append(kal.p[max(kal.x.keys())])
+        return x, p
 
     def fit(self, u, z):
         u = np.array(u)
@@ -430,7 +431,7 @@ class Particle(object):
         self.predicted_X_err = {}
 
         self.Particles = {}
-        self.History ={}
+        self.History = {}
         self.Weights = {}
 
         for n in range(self.N):
@@ -442,9 +443,8 @@ class Particle(object):
         else:
             t = max(self.X.keys())+1
 
-        self.X.update({t : np.mean(self.Particles.values(), axis=0)})
-        self.X_err.update({t : np.std(self.Particles.values(), axis=0)})
-
+        self.X.update({t: np.mean(self.Particles.values(), axis=0)})
+        self.X_err.update({t: np.std(self.Particles.values(), axis=0)})
         # self.history_update()
 
     def history_update(self):
@@ -452,18 +452,16 @@ class Particle(object):
                                                         np.std(self.Particles.values(), axis=0),
                                                         np.mean(self.Weights.values(), axis=0),
                                                         np.std(self.Weights.values(), axis=0)]})
+
     def predict(self, u):
         if len(self.X.keys()) == 0:
             t = 0
         else:
             t = max(self.X.keys())+1
-
         for i in self.Particles.keys():
-            # x_ = np.dot(self.A, self.Particles[i]) + np.dot(self.B, u)
-            self.Particles.update({i: ss.multivariate_normal(mean=np.dot(self.A, self.Particles[i])
-                                                                  + np.dot(self.B, u),
+            self.Particles.update({i: ss.multivariate_normal(mean=np.dot(self.A, self.Particles[i]) + np.dot(self.B, u),
                                                              cov=self.Q).rvs()})
-        
+
         self.predicted_X.update({t : np.mean(self.Particles.values(), axis=0)})
         self.predicted_X_err.update({t : np.std(self.Particles.values(), axis=0)})
         return self.Particles
@@ -474,8 +472,10 @@ class Particle(object):
         else:
             t = max(self.X.keys())+1
 
+        dist = ss.multivariate_normal(mean=np.zeros_like(z), cov=self.R)
         for i in self.Weights.keys():
-            self.Weights.update({i :ss.multivariate_normal(mean= np.dot(self.C, self.Particles[i]), cov=self.R).pdf(z)})
+            dist.mean = np.dot(self.C, self.Particles[i])
+            self.Weights.update({i: dist.pdf(z)})
 
         w = np.sum(self.Weights.values())
         if w > 0:
@@ -484,134 +484,33 @@ class Particle(object):
         else:
             raise ValueError('Sum of weights is smaller than zero. No further Particle computation possible.')
 
-        borders = np.cumsum(self.Weights.values())
-        new = {}
-        for i in self.Particles.keys():
-            sample = np.random.rand()
+        idx = np.sum(np.array(np.tile(np.cumsum(self.Weights.values()), self.N).reshape((self.N, -1)) <
+                     np.tile(np.random.rand(self.N), self.N).reshape((self.N, -1)).T, dtype=int), axis=1)
+        print(len(set(idx)))
+        values = self.Particles.values()
+        for i, j in enumerate(idx):
             try:
-                idx = list(borders > sample).index(True)
-            except ValueError:
-                idx = len(self.Particles.keys())
-            new.update({i: self.Particles[idx]})
-        self.Particles = new.copy()
-        # self.history_update()
-        self.X.update({t : np.mean(self.Particles.values(), axis=0)})
-        self.X_err.update({t : np.std(self.Particles.values(), axis=0)})
+                self.Particles.update({i: values[j]})
+            except IndexError:
+                self.Particles.update({i: values[j-1]})
+
+        self.X.update({t: np.mean(self.Particles.values(), axis=0)})
+        self.X_err.update({t: np.std(self.Particles.values(), axis=0)})
+
         return self.Particles
 
     def fit(self, u, z):
         for i in range(len(u)):
+
             self.predict(u[i])
+
+            try:
+                self.update(z[i])
+            except ValueError:
+                break
             self.History.update({i: self.Particles.values()})
-            self.update(z[i])
-            print(np.amin(self.Weights.values()))
+
         return np.array(self.X.values()),\
                np.array(self.X_err.values()),\
                np.array(self.predicted_X.values()),\
                np.array(self.predicted_X_err.values())
-
-
-if __name__ == '__main__':
-    import matplotlib.pyplot as plt
-    import clickpoints
-    import scipy.optimize as opt
-
-    db = clickpoints.DataFile("click0.cdb")
-    tracks = db.getTracks()
-    points = []
-    for t in tracks:
-        t = t.points_corrected
-        if t.shape[0] > 2:
-            points.append(t)
-
-    # measurements = points[-4][:]
-    D=2*np.random.randint(0,2,size=100)-1
-    X=[[0,0]]
-    for i,d in enumerate(D):
-        X.append([i,X[-1][1]+np.random.normal(loc=d,scale=0.02)])
-    measurements = 50*np.array(X)
-    # measurements = np.array([range(40), range(40)]).T*10
-    # measurements = np.random.normal(measurements, scale=2)
-    #measurements[:, 1] = 0
-    v = measurements[1]-measurements[0]
-    X = np.array([measurements[0][0], v[0], measurements[0][1], v[1]])  # initial state (location and velocity)
-    U = np.zeros([measurements.shape[0], 4])  # external motion
-
-    A = np.array([[1., 1., 0., 0.],
-                  [0., 0., 0., 0.],
-                  [0., 0., 1., 1.],
-                  [0., 0., 0., 0.]])  # next state function
-
-    B = np.zeros_like(A)  # next state function for control parameter
-    C = np.array([[1., 0., 0., 0.],
-                  [0., 0., 1., 0.]])  # measurement function
-    G = np.array([[0, 0],
-                  [1, 0],
-                  [0, 0],
-                  [0, 1]])
-
-    #optimal = opt.minimize_scalar(loger, bounds=[5, 39], method='Bounded')
-
-    ucty = 2e3#10.26#optimal['x']
-    xy_uncty = ucty
-    vxvy_uncty = ucty
-    meas_uncty = 10
-    P = np.diag([ucty, ucty, ucty, ucty])  # initial uncertainty
-    # Q = np.diag([vxvy_uncty, vxvy_uncty])  # Prediction uncertainty
-    Q = np.diag([vxvy_uncty, vxvy_uncty, vxvy_uncty, vxvy_uncty])  # Prediction uncertainty
-    # Q = np.diag([1, vxvy_uncty, 1, vxvy_uncty])  # Prediction uncertainty
-    R = np.diag([meas_uncty, meas_uncty])  # Measurement uncertainty
-
-    Part = Particle(1000, A, B, C, Q, R, [X])
-
-    X, X_err, Pred, Pred_err = Part.fit(U, measurements)
-
-    plt.errorbar(X.T[0], X.T[2], xerr=X_err.T[0], yerr=X_err.T[2], c='b')
-    plt.errorbar(Pred.T[0], Pred.T[2], xerr=Pred_err.T[0], yerr=Pred_err.T[2], c='r')
-    plt.axis('equal')
-    plt.show()
-
-    #
-    # start_point = np.array([p[0] for p in points])
-    # second_point = np.array([p[1] for p in points])
-    # V = second_point - start_point
-    # X = np.array([start_point.T[0], V.T[0], start_point.T[1], V.T[1]]).T
-    # P = [P for v in V]
-    # MultiKal = MultiKalman(A, B, C, G, Q, R, X, P, lag=5, learn_time=5)
-    #
-    # maxLen = max([p.shape[0] for p in points])
-    #
-    # timeline = np.array([[p[0] for p in points]])
-    #
-    # for i in range(maxLen):
-    #     timepoint = []
-    #     for p in points:
-    #         try:
-    #             meas = p[i]
-    #         except IndexError:
-    #             meas = [np.nan, np.nan]
-    #         if np.random.rand < 0.5:
-    #             timepoint.append(meas)
-    #         else:
-    #             meas=[meas]
-    #             meas.extend(timepoint)
-    #             timepoint = meas
-    #     timeline = np.vstack((timeline, [timepoint]))
-    #
-    # # timeline = timeline[:45]
-    #
-    # UU = np.zeros((timeline.shape[0], timeline.shape[1], 4))
-    #
-    # X_Post, P_Post, Pred, Pred_err = MultiKal.fit(UU, timeline)
-    #
-    # for i in range(5):
-    #     plt.errorbar(Pred[i].T[0], Pred[i].T[2], color='r')#, xerr=P_Post.T[0, 0][i], yerr=P_Post.T[2, 2][i], color="b")
-    #     plt.errorbar(np.array(MultiKal.Filters[i].z.values()).T[0], np.array(MultiKal.Filters[i].z.values()).T[1], color='b')#, xerr=Pred_err.T[0,0][i], yerr=Pred_err.T[2,2][i], color="r")
-    #     plt.axis('equal')
-    # # plt.show()
-    #     plt.savefig("/home/alex/Desktop/number%s.png"%i)
-    #     plt.clf()
-
-
-
-
---
+++ TestFilters.py
@@ -0,0 +1,104 @@
+if __name__ == '__main__':
+    import numpy as np
+    from Kalman import Particle
+    import matplotlib.pyplot as plt
+    import clickpoints
+    import scipy.optimize as opt
+
+    db = clickpoints.DataFile("click0.cdb")
+    tracks = db.getTracks()
+    points = []
+    for t in tracks:
+        t = t.points_corrected
+        if t.shape[0] > 2:
+            points.append(t)
+
+    # measurements = points[-1][:]
+    D = 2*np.random.randint(0, 2, size=100)-1
+    X = [[0, 0]]
+    for i, d in enumerate(D):
+        X.append([i, X[-1][1]+np.random.normal(loc=d, scale=0.1)])
+    measurements = np.array(X)
+    # measurements = np.array([range(40), range(40)]).T*10
+    # measurements = np.random.normal(measurements, scale=2)
+    #measurements[:, 1] = 0
+    v = measurements[1]-measurements[0]
+    X = np.array([measurements[0][0], v[0], measurements[0][1], v[1]])  # initial state (location and velocity)
+    U = np.zeros([measurements.shape[0], 4])  # external motion
+
+    A = np.array([[1., 1., 0., 0.],
+                  [0., 1., 0., 0.],
+                  [0., 0., 1., 1.],
+                  [0., 0., 0., 0.]])  # next state function
+
+    B = np.zeros_like(A)  # next state function for control parameter
+    C = np.array([[1., 0., 0., 0.],
+                  [0., 0., 1., 0.]])  # measurement function
+    G = np.array([[0, 0],
+                  [1, 0],
+                  [0, 0],
+                  [0, 1]])
+
+    #optimal = opt.minimize_scalar(loger, bounds=[5, 39], method='Bounded')
+
+    ucty = 1#10.26#optimal['x']
+    xy_uncty = ucty
+    vxvy_uncty = ucty
+    meas_uncty = 0.1
+    P = np.diag([ucty, ucty, ucty, ucty])  # initial uncertainty
+    # Q = np.diag([vxvy_uncty, vxvy_uncty])  # Prediction uncertainty
+    # Q = np.diag([vxvy_uncty, vxvy_uncty, vxvy_uncty, vxvy_uncty])  # Prediction uncertainty
+    Q = np.diag([0., vxvy_uncty, 0., vxvy_uncty])  # Prediction uncertainty
+    R = np.diag([meas_uncty, meas_uncty])  # Measurement uncertainty
+
+    Part = Particle(100, A, B, C, Q, R, [X])
+
+    X, X_err, Pred, Pred_err = Part.fit(U, measurements)
+
+    # plt.errorbar(X.T[0], X.T[2], xerr=X_err.T[0], yerr=X_err.T[2], c='b')
+    plt.errorbar(Pred.T[0], Pred.T[2], xerr=Pred_err.T[0], yerr=Pred_err.T[2], c='r')
+    plt.errorbar(measurements.T[0], measurements.T[1], xerr=meas_uncty, yerr=meas_uncty, c='b')
+    plt.axis('equal')
+    plt.show()
+
+    #
+    # start_point = np.array([p[0] for p in points])
+    # second_point = np.array([p[1] for p in points])
+    # V = second_point - start_point
+    # X = np.array([start_point.T[0], V.T[0], start_point.T[1], V.T[1]]).T
+    # P = [P for v in V]
+    # MultiKal = MultiKalman(A, B, C, G, Q, R, X, P, lag=5, learn_time=5)
+    #
+    # maxLen = max([p.shape[0] for p in points])
+    #
+    # timeline = np.array([[p[0] for p in points]])
+    #
+    # for i in range(maxLen):
+    #     timepoint = []
+    #     for p in points:
+    #         try:
+    #             meas = p[i]
+    #         except IndexError:
+    #             meas = [np.nan, np.nan]
+    #         if np.random.rand < 0.5:
+    #             timepoint.append(meas)
+    #         else:
+    #             meas=[meas]
+    #             meas.extend(timepoint)
+    #             timepoint = meas
+    #     timeline = np.vstack((timeline, [timepoint]))
+    #
+    # # timeline = timeline[:45]
+    #
+    # UU = np.zeros((timeline.shape[0], timeline.shape[1], 4))
+    #
+    # X_Post, P_Post, Pred, Pred_err = MultiKal.fit(UU, timeline)
+    #
+    # for i in range(5):
+    #     plt.errorbar(Pred[i].T[0], Pred[i].T[2], color='r')#, xerr=P_Post.T[0, 0][i], yerr=P_Post.T[2, 2][i], color="b")
+    #     plt.errorbar(np.array(MultiKal.Filters[i].z.values()).T[0], np.array(MultiKal.Filters[i].z.values()).T[1], color='b')#, xerr=Pred_err.T[0,0][i], yerr=Pred_err.T[2,2][i], color="r")
+    #     plt.axis('equal')
+    # # plt.show()
+    #     plt.savefig("/home/alex/Desktop/number%s.png"%i)
+    #     plt.clf()
+
