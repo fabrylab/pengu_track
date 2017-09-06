89f27237aa3dbf6a6743d17cbba3215468236db7
Alex <55278900+AlexanderWinterl@users.noreply.github.com>
2017-09-06 09:46:47 +0200

    * Distance_And_Resolution: Changed DB-Paths and added the birdflight plot * ROC_Mockup: Changed plotting size for thesis

--- PenguTrack/Tools/ROC_Mockup.py
+++ PenguTrack/Tools/ROC_Mockup.py
@@ -10,6 +10,7 @@ from PenguTrack.DataFileExtended import DataFileExtended
 import matplotlib.pyplot as plt
 import seaborn as sn
 import my_plot
+sn.set_style("white")
 
 fig, ax = plt.subplots()
 ax.set_xlim([-0.05, 1.05])
@@ -40,28 +41,30 @@ ax.fill_between(data[3][2], data[3][1], color = c[2], alpha=0.2)
 ax.legend(loc="best", prop={'size':12})
 my_plot.despine(ax)
 my_plot.setAxisSizeMM(fig, ax, 147, 90)
-plt.savefig("/home/birdflight/Desktop/Mock_ROC.pdf")
-plt.savefig("/home/birdflight/Desktop/Mock_ROC.png")
+plt.savefig("/home/alex/Desktop/Mock_ROC.pdf")
+plt.savefig("/home/alex/Desktop/Mock_ROC.png")
 
 
 fig, ax = plt.subplots()
-ax.set_xlim([-0.05, 10.05])
+ax.set_xlim([-0.05, 9.05])
 ax.set_ylim([-0.05, 1.05])
 ax.set_xlabel("Classifier Parameter")
 ax.set_ylabel("True Positive Rate")
+plts = []
 for n in [1,2,3]:
-    ax.plot(data[n][0], data[n][1], '-o', color=c[n - 1], label=label[n].split(" ")[0])
+    p,=ax.plot(data[n][0], data[n][1], '-o', color=c[n - 1], label=label[n].split(" ")[0])
+    plts.append(p)
 # ax.fill_between(data[][0], data[3][1], color = c[2], alpha=0.2)
-ax.legend(loc="best",ncol=1 , prop={'size':12})
+ax.legend(loc="best",ncol=2 , prop={'size':10})
 my_plot.despine(ax)
-my_plot.setAxisSizeMM(fig, ax, 147, 90)
+my_plot.setAxisSizeMM(fig, ax, 147/2, 90/2)
 plt.tight_layout()
-plt.savefig("/home/birdflight/Desktop/Mock_TPR.pdf")
-plt.savefig("/home/birdflight/Desktop/Mock_TPR.png")
+plt.savefig("/home/alex/Desktop/Mock_TPR.pdf")
+plt.savefig("/home/alex/Desktop/Mock_TPR.png")
 
 
 fig, ax = plt.subplots()
-ax.set_xlim([-0.05, 10.05])
+ax.set_xlim([-0.05, 9.05])
 ax.set_ylim([-0.05, 1.05])
 ax.set_xlabel("Classifier Parameter")
 ax.set_ylabel("Precision")
@@ -71,11 +74,11 @@ for n in [1,2,3]:
     prec[np.isnan(prec)]=1.
     ax.plot(data[n][0], prec, '-o', color=c[n - 1], label=label[n].split(" ")[0])
 # ax.fill_between(data[][0], data[3][1], color = c[2], alpha=0.2)
-ax.legend(loc="best", ncol=1 ,prop={'size': 12})
+legend = ax.legend(loc="best", ncol=2 ,prop={'size': 10})#, bbox_to_anchor=(1.,0.))
 my_plot.despine(ax)
-my_plot.setAxisSizeMM(fig, ax, 147, 90)
+my_plot.setAxisSizeMM(fig, ax, 147/2, 90/2)
 plt.tight_layout()
-plt.savefig("/home/birdflight/Desktop/Mock_PRE.pdf")
-plt.savefig("/home/birdflight/Desktop/Mock_PRE.png")
+plt.savefig("/home/alex/Desktop/Mock_PRE.pdf")
+plt.savefig("/home/alex/Desktop/Mock_PRE.png")
 
 plt.show()
